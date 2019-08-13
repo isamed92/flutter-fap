6abaeb5f647c3aecc6f777f15529c5d401313944
IsaiasPC <isamed92@gmail.com>
2019-08-13 17:15:55 -0300

    style property added in home_page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  
+  final estiloTexto = new TextStyle(fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,7 +13,12 @@ class HomePage extends StatelessWidget {
         title: Text('Titulo'),
       ),
       body: Center(
-        child: Text('hola mundo'),
+        child: Column(children: <Widget>[
+          Text('Numero de clicks:', style:estiloTexto,),
+          Text('0', style:estiloTexto),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     );
   }
